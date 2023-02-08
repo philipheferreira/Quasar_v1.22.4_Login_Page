659c97d6ff1a4be752be0858dd56c05ad7598cc4
philipheferreira <philipheferreira@gmail.com>
2023-02-08 11:55:51 -0300

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="">
+      <q-input outlined v-model="usuario" label="usuario" /> 
+      <q-input outlined v-model="senha" label="Password" />
+      <button class="" style="background: #FF0080; color: white;" >Login</button>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex', 
+  data () {
+    return {
+      usuario: '',
+      senha: ''
+    }
+  }
+}
+</script>
