8f6e22faa947d1f72245fe64e5d29b7c9cdd05f8
philipheferreira <philipheferreira@gmail.com>
2023-03-24 15:17:04 -0300

    add login and cadastro options

--- src/pages/loginTela.vue
+++ src/pages/loginTela.vue
@@ -1,16 +1,31 @@
 <template>
   <q-page>
     <div class="q-pa-md column items-center">
-      <div style="min-width: 400px;">
-        <h3 class="q-ma-md text-center">Login</h3>
-        <q-input class="q-ma-md" outlined v-model="usuario" label="usuario" /> 
-        <q-input class="q-ma-md" outlined v-model="senha" label="Password" />
-        <p class="text-center">esqueceu a senha?</p>
-        <div class="text-center">
-          <q-checkbox class="q-ma-md" v-model="valorCheckbox" label="Lembre-se"></q-checkbox>
-          <button class="q-ma-md" style="background: #FF0080; color: white;" >Login</button>
-        </div>
+      <q-card class="meuCard">
+      <div style="min-width: 500px;">
+        <q-tabs v-model="cardMenu" >
+          <q-tab label="Login" name="login" />
+          <q-tab label="Cadastro" name="cadastro" />
+        </q-tabs>
+        <q-separator/>
+
+        <q-tab-panels v-model="cardMenu" animated>
+          <q-tab-panel name="login">
+            <h3 class="q-ma-md text-center">Login</h3>
+            <q-input class="q-ma-md" outlined v-model="usuario" label="usuario" /> 
+            <q-input class="q-ma-md" outlined v-model="senha" label="Password" />
+            <p class="text-center">esqueceu a senha?</p>
+            <div class="text-center">
+              <q-checkbox class="q-ma-md" v-model="valorCheckbox" label="Lembre-se"></q-checkbox>
+              <q-btn align="between" class="q-ma-md" icon="login" label="login" style="background: #FF0080; color: white;"/>
+            </div>
+          </q-tab-panel>
+          <q-tab-panel name="cadastro">
+            
+          </q-tab-panel>
+        </q-tab-panels>
       </div>
+    </q-card>
     </div>
   </q-page>
 </template>
@@ -22,7 +37,8 @@ export default {
     return {
       usuario: '',
       senha: '', 
-      valorCheckbox: false
+      valorCheckbox: false,
+      cardMenu: 'login'
     }
   }
 }
