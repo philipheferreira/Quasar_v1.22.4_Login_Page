532f61a0464bd648e05fa1648ab4017faeedee31
philipheferreira <philipheferreira@gmail.com>
2023-03-24 14:34:56 -0300

    center login

--- src/pages/loginTela.vue
+++ src/pages/loginTela.vue
@@ -1,12 +1,15 @@
 <template>
   <q-page>
-    <div>
-      <h3 class="q-ma-md">Login</h3>
-      <q-input class="q-ma-md" outlined v-model="usuario" label="usuario" /> 
-      <q-input class="q-ma-md" outlined v-model="senha" label="Password" />
-      <div>
-        <q-checkbox class="q-ma-md" v-model="valorCheckbox" label="Lembre-se"></q-checkbox>
-        <button class="q-ma-md" style="background: #FF0080; color: white;" >Login</button>
+    <div class="q-pa-md column items-center">
+      <div style="min-width: 400px;">
+        <h3 class="q-ma-md text-center">Login</h3>
+        <q-input class="q-ma-md" outlined v-model="usuario" label="usuario" /> 
+        <q-input class="q-ma-md" outlined v-model="senha" label="Password" />
+        <p class="text-center">esqueceu a senha?</p>
+        <div class="text-center">
+          <q-checkbox class="q-ma-md" v-model="valorCheckbox" label="Lembre-se"></q-checkbox>
+          <button class="q-ma-md" style="background: #FF0080; color: white;" >Login</button>
+        </div>
       </div>
     </div>
   </q-page>
